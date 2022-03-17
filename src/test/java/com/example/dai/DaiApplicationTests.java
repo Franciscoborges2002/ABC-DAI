2a4bf9ce2ab415017e0cfb4f66ceea6aa52b4c4d
Francisco Borges <franciscomsborges2002@gmail.com>
2022-03-17 16:29:31 +0000

    Start of the project

---
+++ src/test/java/com/example/dai/DaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
